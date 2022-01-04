676d1c4859918743915664350a1358883ba31b11
EmrhSGLM <emrh.sglm7168@gmail.com>
2022-01-04 13:09:34 +0300

    04/01/2022__groupWorking

--- src/proje/Main.java
+++ src/proje/Main.java
@@ -1,4 +1,132 @@
 package proje;
 
+import java.util.*;
+
 public class Main {
+    static List<Ogrenci> list=new ArrayList<Ogrenci>();
+    static List<Ogrenci> list1=new ArrayList<Ogrenci>();
+
+    public static void main(String[] args) {
+        ekleme();
+        System.out.println("Task 03 -> Öğrenci notlarını alt ve üst limitlere göre sıralayarak  yazdirin\n");
+        ogrNotlariAltUstSınırSırala(75,91);
+        System.out.println("\nTask 04 -> Öğrencileri yaşlarına göre sıralayark yazdırin\n");
+        ogrYasSıra();
+        System.out.println("\nTask 05 -> Öğrencileri belirli bir yaşa ve cinsiyete göre sıralayarak listeleyin\n");
+        ogrYasCinsiyetGoreSirala(49,"e");
+        System.out.println("\nTask 06 -> Öğrencileri isimlerine göre yazdırın\n");
+        ogrIsimSıralaYazdir();
+        System.out.println("\nTask 07 -> Öğrencileri soyisimlerine göre yazdırin\n");
+        ogrSoyAdSiralaYazdir();
+        System.out.println("\nTask 08 -> Öğrencileri cinsiyetlerine göre yazdıran\n");
+        ogrCinsiyetGoreYazdir("k");
+        System.out.println("\nTask 09 -> listeyi yazdırırken içinde veri var mı diye kontrol edin\n");
+        listeBosDoluKontrol();
+        System.out.println("\nTask 10 -> reduce metoduyla ortalama hesaplayin\n");
+        ogrDiplomaNotOrtHesaplama();
+        ogrDiplomaNotOrtHesaplama1();
+        System.out.println("\nTask 11 -> Girilen ogrenci ismini sil\n");
+        girilenOgrAdiSil();
+
+    }
+
+
+
+    private static void girilenOgrAdiSil() {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter name which wanna delete ");
+        String name=scan.next();
+
+        // Bir Collection'daki veriler stream'e çevirmeksizin removeIf() metoduyla silinebilir.
+        // removeIf bir lamba fonksiyonunu parametre olarak alabilir.
+        // removeIf bir ArrayList metodudur ve listeyi kalıcı olarak günceler.
+
+        boolean silindiMi = list.removeIf(t-> t.getAd().equalsIgnoreCase(name));
+        System.out.println( silindiMi ? "Basarili" : "Basarisiz");
+
+    }
+
+    private static void ogrDiplomaNotOrtHesaplama1() {
+        System.out.println("2. Yontem => "+list.
+                stream().
+                mapToDouble(t -> t.getDiplomaNotu()).
+                average());
+    }
+
+    private static void ogrDiplomaNotOrtHesaplama() {
+        Double toplam =list.
+                stream().
+                map(t->t.getDiplomaNotu()).
+                reduce(Double::sum).orElse(0.0);
+
+        System.out.println("1. Yontem => "+toplam / list.size());
+
+    }
+    private static void listeBosDoluKontrol() {
+        // class level da olusturdugum static elemanlı ve elemansiz iki listeyi
+        // ternary kullanarak bos olup olmadigini kontrol ettim
+        System.out.println(list.isEmpty() ? "Liste Bos" : list.toString());
+        System.out.println(list1.isEmpty() ? "Liste Bos" : list1.toString());
+    }
+
+    private static void ogrCinsiyetGoreYazdir(String cinsiyet) {
+        list.
+                stream().
+                filter(t->t.getCinsiyet().equalsIgnoreCase(cinsiyet)).
+                forEach(System.out::println);
+    }
+
+    private static void ogrSoyAdSiralaYazdir() {
+        list.
+                stream().
+                sorted(Comparator.comparing(Ogrenci::getSoyAd)).
+                forEach(System.out::println);
+    }
+
+    private static void ogrIsimSıralaYazdir() {
+        list.
+                stream().
+                sorted(Comparator.comparing(Ogrenci::getAd)).
+                forEach(System.out::println);
+    }
+
+    private static void ogrYasCinsiyetGoreSirala(int yas, String cinsiyet) {
+        list.
+                stream().
+                filter(t-> t.getYas() == yas && t.getCinsiyet().equalsIgnoreCase(cinsiyet)).
+                forEach(System.out::println);
+
+    }
+
+    private static void ogrYasSıra() {
+        list.
+                stream().
+                sorted(Comparator.comparing(Ogrenci::getYas)).
+                //forEach(System.out::println);
+                forEach(t-> System.out.println(t.toString()));
+    }
+
+    private static void ogrNotlariAltUstSınırSırala(int alt , int ust) {
+        list.
+                stream().
+                filter(t-> t.getDiplomaNotu() > alt && t.getDiplomaNotu() < ust).
+                sorted(Comparator.comparing(t->t.getDiplomaNotu())).
+                //sorted(Comparator.comparing(Ogrenci::getDiplomaNotu)).
+                forEach(t-> System.out.println(t.toString()));
+    }
+
+
+    public static List<Ogrenci> ekleme() {
+
+        list.add(new Ogrenci("Hacer", "Turmak", 30, 92, "k"));
+        list.add(new Ogrenci("Zuhal", "Eren", 32, 90, "k"));
+        list.add(new Ogrenci("Emrah", "Saglam", 34, 70, "e"));
+        list.add(new Ogrenci("Hadi", "Arici", 45, 85, "e"));
+        list.add(new Ogrenci("Hikmet", "Ertem", 49, 82, "e"));
+        list.add(new Ogrenci("Emre", "Gul", 32, 90, "e"));
+
+        return list;
+    }
+
 }
--- src/proje/Ogrenci.java
+++ src/proje/Ogrenci.java
@@ -6,12 +6,77 @@ public class Ogrenci {
     // Task 02 -> toString methodu ile ogrenci bilgilerin yazdirin
     // Task 03 -> Öğrenci notlarını alt ve üst limitlere göre sıralayarak  yazdirin
     // Task 04 -> Öğrencileri yaşlarına göre sıralayark yazdırin
-    // Task 04 -> Öğrencileri belirli bir yaşa ve cinsiyete göre sıralayarak listeleyin
-    // Task 05 -> Öğrencileri isimlerine göre yazdırın
-    // Task 06 -> Öğrencileri soyisimlerine göre yazdırin
-    // Task 07 -> Öğrencileri cinsiyetlerine göre yazdıran
-    // Task 08 -> listeyi yazdırırken içinde veri var mı diye kontrol edin
-    // Task 09 -> reduce metoduyla ortalama hesaplayin
-    // Task 10 -> Girilen ogrenci ismini sil
-    // Task 11 -> Test amaçlı Öğrenci nesneleri oluşturun.
+    // Task 05 -> Öğrencileri belirli bir yaşa ve cinsiyete göre sıralayarak listeleyin
+    // Task 06 -> Öğrencileri isimlerine göre yazdırın
+    // Task 07 -> Öğrencileri soyisimlerine göre yazdırin
+    // Task 08 -> Öğrencileri cinsiyetlerine göre yazdıran
+    // Task 09 -> listeyi yazdırırken içinde veri var mı diye kontrol edin
+    // Task 10 -> reduce metoduyla ortalama hesaplayin
+    // Task 11 -> Girilen ogrenci ismini sil
+    // Task 12 -> Test amaçlı Öğrenci nesneleri oluşturun.
+
+    String ad;
+    String soyAd;
+    int yas;
+    double diplomaNotu;
+    String cinsiyet;
+
+    @Override
+    public String toString() {
+        return "Ogrenci{" +
+                "ad='" + ad + '\'' +
+                ", soyAd='" + soyAd + '\'' +
+                ", yas=" + yas +
+                ", diplomaNotu=" + diplomaNotu +
+                ", cinsiyet='" + cinsiyet + '\'' +
+                '}';
+    }
+
+    public Ogrenci(String ad, String soyAd, int yas, double diplomaNotu, String cinsiyet) {
+        this.ad = ad;
+        this.soyAd = soyAd;
+        this.yas = yas;
+        this.diplomaNotu = diplomaNotu;
+        this.cinsiyet = cinsiyet;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyAd() {
+        return soyAd;
+    }
+
+    public void setSoyAd(String soyAd) {
+        this.soyAd = soyAd;
+    }
+
+    public int getYas() {
+        return yas;
+    }
+
+    public void setYas(int yas) {
+        this.yas = yas;
+    }
+
+    public double getDiplomaNotu() {
+        return diplomaNotu;
+    }
+
+    public void setDiplomaNotu(double diplomaNotu) {
+        this.diplomaNotu = diplomaNotu;
+    }
+
+    public String getCinsiyet() {
+        return cinsiyet;
+    }
+
+    public void setCinsiyet(String cinsiyet) {
+        this.cinsiyet = cinsiyet;
+    }
 }
